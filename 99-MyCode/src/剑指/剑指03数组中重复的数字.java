e9069ebd58b7418714aeda32cff06df4e98cd9e9
laucherish <laucherish@163.com>
2020-08-03 16:52:02 +0800

    Add LeetCode.

---
+++ 99-MyCode/src/剑指/剑指03数组中重复的数字.java
@@ -0,0 +1,50 @@
+package 剑指;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * 找出数组中重复的数字。
+ *
+ *
+ * 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+ *
+ * 示例 1：
+ *
+ * 输入：
+ * [2, 3, 1, 0, 2, 5, 3]
+ * 输出：2 或 3
+ *  
+ *
+ * 限制：
+ *
+ * 2 <= n <= 100000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class 剑指03数组中重复的数字 {
+    public int findRepeatNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int repeat = -1;
+        for (int num : nums) {
+            if (!set.add(num)) {
+                repeat = num;
+                break;
+            }
+        }
+        return repeat;
+    }
+
+    public int findRepeatNumber2(int[] nums) {
+        int[] tmp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            tmp[nums[i]]++;
+            if (tmp[nums[i]]==2) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
