f3710328c956fa8027f816e27a6ca447899783b8
laucherish <laucherish@163.com>
2020-08-03 20:30:18 +0800

    Add Set.

---
+++ 10-集合/src/com/mj/list/LinkedList.java
@@ -0,0 +1,188 @@
+package com.mj.list;
+
+/**
+ * 双向链表
+ *
+ * @param <E>
+ */
+public class LinkedList<E> extends AbstractList<E> {
+
+    private Node<E> first;
+    private Node<E> last;
+
+    @Override
+    public void clear() {
+        size = 0;
+        first = null;
+        last = null;
+    }
+
+    @Override
+    public void add(E element) {
+        add(size, element);
+    }
+
+    @Override
+    public E get(int index) {
+        return node(index).element;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        Node<E> node = node(index);
+        E old = node.element;
+        node.element = element;
+        return old;
+    }
+
+    /**
+     * 编写链表要注意边界测试，比如0，index-1，index
+     *
+     * @param index
+     * @param element
+     */
+    @Override
+    public void add(int index, E element) {
+        rangeCheckForAdd(index);
+
+        if (index == size) { // 往最后面添加元素
+            Node<E> oldLast = last;
+            last = new Node<>(oldLast, element, null);
+            if (oldLast == null) {
+                first = last;
+            } else {
+                oldLast.next = last;
+            }
+        } else {
+            Node<E> next = node(index);
+            Node<E> prev = next.prev;
+            Node<E> node = new Node<>(prev, element, next);
+            next.prev = node;
+            if (prev == null) { // index == 0
+                first = node;
+            } else {
+                prev.next = node;
+            }
+        }
+        size++;
+    }
+
+    @Override
+    public E remove(int index) {
+        rangeCheck(index);
+
+        Node<E> node = node(index);
+        Node<E> prev = node.prev;
+        Node<E> next = node.next;
+
+        if (prev == null) {  // index == 0
+            first = next;
+        } else {
+            prev.next = next;
+        }
+        if (next == null) { // index == size - 1
+            last = prev;
+        } else {
+            next.prev = prev;
+        }
+
+        size--;
+        return node.element;
+    }
+
+    @Override
+    public int indexOf(E element) {
+        if (element == null) {
+            Node<E> node = first;
+            for (int i = 0; i < size; i++) {
+                if (node.element == null) {
+                    return i;
+                }
+                node = node.next;
+            }
+        } else {
+            Node<E> node = first;
+            for (int i = 0; i < size; i++) {
+                if (element.equals(node.element)) {
+                    return i;
+                }
+                node = node.next;
+            }
+        }
+        return ELEMENT_NOT_FOUND;
+    }
+
+    /**
+     * 获取index位置节点对象
+     *
+     * @param index
+     * @return
+     */
+    private Node<E> node(int index) {
+        rangeCheck(index);
+        if (index < (size >> 1)) {
+            Node<E> node = first;
+            for (int i = 0; i < index; i++) {
+                node = node.next;
+            }
+            return node;
+        } else {
+            Node<E> node = last;
+            for (int i = size - 1; i > index; i--) {
+                node = node.prev;
+            }
+            return node;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("LinkedList:[");
+        Node<E> p = first;
+        for (int i = 0; i < size; i++) {
+            if (i == size - 1) {
+                builder.append(p.toString());
+            } else {
+                builder.append(p.toString()).append(",");
+            }
+            p = p.next;
+        }
+        builder.append("]");
+
+        // 遍历链表的方法
+//        Node<E> node1 = first;
+//        while (node1 != null) {
+//            node1 = node1.next;
+//        }
+
+        return builder.toString();
+    }
+
+    private static class Node<E> {
+        E element;
+        Node<E> prev;
+        Node<E> next;
+
+        public Node(Node<E> prev, E element, Node<E> next) {
+            this.prev = prev;
+            this.element = element;
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder builder = new StringBuilder();
+
+            if (prev != null) {
+                builder.append(prev.element);
+            }
+            builder.append("_").append(element).append("_");
+            if (next != null) {
+                builder.append(next.element);
+            }
+
+            return builder.toString();
+        }
+    }
+}
